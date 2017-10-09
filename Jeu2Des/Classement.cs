136ef5d17ccfc04417fd33167ba49179d05bc64f
Stephan <stephan.urthaler@2isa.org>
2017-10-09 13:35:05 +0200

    Ajout commentaire constructeur classement

--- Jeu2Des/Classement.cs
+++ Jeu2Des/Classement.cs
@@ -24,6 +24,7 @@ namespace Jeu2Des
             set { _ListeEntrees = value; }
         }
 
+        //Constructor
         protected Classement()
         {
             _ListeEntrees = new List<Entree>();
